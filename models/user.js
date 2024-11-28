03e624d6cb4e7d8d438fbb2a8f28519b19bdce5d
fares <faresbenromdhank@gmail.com>
2024-11-28 15:10:47 +0100

    u

--- models/user.js
+++ models/user.js
@@ -5,8 +5,6 @@ const UserSchema = new Schema({
   nom: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role:{type:String,required:true},
-  votes: [{ type: Schema.Types.ObjectId, ref: 'Vote' }]
 });
 
 module.exports = mongoose.model('User', UserSchema);
